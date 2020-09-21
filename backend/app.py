861c9e9021a63f5f97b0ab0d023df99b70b4e172
CallumTipper <callum.tipper@gmail.com>
2020-09-21 12:46:01 +0100

    ordered by main question number, solved the tutor adding an answer button problem

--- backend/app.py
+++ backend/app.py
@@ -29,11 +29,16 @@ def index():
 
 @app.route('/student')
 def student():
+    global ans
+    ans = sorted(ans, key=lambda k: k['number'])
     return render_template("student.html", ans = ans)
 
 @app.route('/student', methods=['POST'])
 def post_q():
+    global ans
+    global question_counter
     unique_id = question_counter + 1
+    question_counter += 1
     number = request.form.get('number')
     section = request.form.get('section')
     question = request.form.get('question')
@@ -52,14 +57,18 @@ def post_q():
     }
 
     ans.append(q.copy())
+    ans = sorted(ans, key=lambda k: k['number'])
     return render_template("student.html", ans = ans)
 
 @app.route('/tutor')
 def tutor():
+    global ans
+    ans = sorted(ans, key=lambda k: k['number'])
     return render_template("tutor.html", ans = ans)
 
 @app.route('/tutor', methods=['POST'])
 def post_a():
+    global ans
     answer = request.form.get('answer')
     q_id = request.form.get('id')
     q_id = int(q_id)
@@ -68,6 +77,7 @@ def post_a():
         if i['id'] == q_id:
             i['answer'] = answer
     
+    ans = sorted(ans, key=lambda k: k['number'])
     return render_template('tutor.html', ans = ans)
 
 if __name__ == "__main__":
--- backend/static/tutor.js
+++ backend/static/tutor.js
@@ -1,21 +1,47 @@
 console.log("hello tutor js")
 
-function showForm(){
-    var Aform = document.getElementById("Aform-hidden")
-    Aform.style.display = "block"
-    Aform.id = "Aform-block"
+function showForm(questionId){
+    // var questionId = this.id.replace(/^\D+/g, '')
+    console.log(typeof questionId)
+    if (typeof questionId === "number") {
+        console.log(111,questionId)
+        console.log(222,typeof questionId)
+        var Aform = document.getElementById("Aform-hidden-"+questionId)
+        Aform.style.display = "block"
+        Aform.id = "Aform-block-"+questionId
   
-    button = document.getElementById("add-button")
-    button.onclick = hideForm
-    button.innerHTML = "Cancel"
+        button = document.getElementById("add-button-"+questionId)
+        button.onclick = hideForm
+        button.innerHTML = "Cancel"
+        console.log("dfone")
+    } else {
+        questionId = questionId.target.id.replace(/^\D+/g, '')
+        console.log(111,questionId)
+        console.log(222,typeof questionId)
+        var Aform = document.getElementById("Aform-hidden-"+questionId)
+        Aform.style.display = "block"
+        Aform.id = "Aform-block-"+questionId
+    
+        button = document.getElementById("add-button-"+questionId)
+        button.onclick = hideForm
+        button.innerHTML = "Cancel"
+        console.log("dfone")
+    }
 }
 
 function hideForm(){
-    var Aform = document.getElementById("Aform-block")
+    console.log(this.id)
+    var questionId = this.id.replace(/^\D+/g, '')
+    console.log(questionId)
+    questionId = questionId.toString()
+    console.log(333,questionId)
+    console.log(444,typeof questionId)
+    var Aform = document.getElementById("Aform-block-"+questionId)
+    console.log(555,Aform)
     Aform.style.display = "none"
-    Aform.id = "Aform-hidden"
+    Aform.id = "Aform-hidden-"+questionId
 
-    button = document.getElementById("add-button")
-    button.onclick=showForm
-    button.innerHTML = "Add Answer"
+    button = document.getElementById("add-button-"+questionId)
+    button.onclick = showForm
+    button.innerHTML = "Answer This Question"
 }
