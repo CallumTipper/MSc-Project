f6a4600c08ba27a2b5c4c25cb5fc2e97f0e92ab9
CallumTipper <callum.tipper@gmail.com>
2020-10-12 19:28:57 +0100

    pushing this small change

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,7 @@ ans = [{
 
 @app.route('/')
 def index():
-    lnk = "https://workshop-faq.herokuapp.com"
+    lnk = ""
     return render_template("index.html", link=lnk)
 
 @app.route('/student')
