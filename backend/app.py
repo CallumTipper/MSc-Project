8aa8672d9555ad77b06cdea8d349ebc60163ed56
CallumTipper <callum.tipper@gmail.com>
2020-10-19 16:12:25 +0100

    data is kept in a txt file json, ready to test on heroku

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,36 @@
 from flask import Flask, render_template, json, request
 from datetime import datetime
-app = Flask(__name__)
 
+
+# Global variables
+app = Flask(__name__)
 time = datetime.now()
 question_counter = 1
 
-ans = []
 
-def sortQuestions(answers):
+
+# Auxilliary functions
+def loadData():
+    with open('data.txt', 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+def writeData(data):
+    with open('data.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+def sortQuestions(ans):
+    return sorted(ans, key=lambda k: k['flag', reverse=True])
+
+def sortAnswers(answers):
     a = sorted(answers, key=lambda k: k['section'])
     b = sorted(answers, key=lambda k: k['number'])
     return b
 
-# App routes for basic page loading on navigation
 
+
+
+# App routes for basic page loading on navigation
 @app.route('/')
 def index():
     lnk = ""
@@ -21,27 +38,25 @@ def index():
 
 @app.route('/student')
 def student():
-    global ans
-
-    answers = sortQuestions(ans)
+    ans = loadData()["ans"]
+    ans = sortQuestions(ans)
+    answers = sortAnswers(ans)
     return render_template("student.html", ans = ans, answers = answers)
 
 @app.route('/tutor')
 def tutor():
-    global ans
-
-    answers = sortQuestions(ans)
+    ans = loadData()["ans"]
+    ans = sortQuestions(ans)
+    answers = sortAnswers(ans)
     return render_template("tutor.html", ans = ans, answers = answers)
 
 
 
 # App routes for POST Requests
-
 @app.route('/student', methods=['POST'])
 def post_q():
-    global ans
     global question_counter
-
+    data = loadData()
     postOrigin = request.form.get("post-origin")
 
     if postOrigin == "post_q":
@@ -68,69 +83,85 @@ def post_q():
         }
 
         print(time, "Question Added {}-{}: '{}' ".format(q["number"], q["section"], q["question"])) # For log purposes
+        
+        data["ans"].append(q)
+        writeData(data)
+        ans = loadData()["ans"]
 
-        ans.append(q.copy())
-
-        answers = sortQuestions(ans)
+        ans = sortQuestions(ans)
+        answers = sortAnswers(ans)
         return render_template("question_submitted.html", ans = ans, answers = answers)
 
     elif postOrigin == "flag":
-        q_id = request.form.get("questionId")
-        q_id = int(q_id)
+        q_id = int(request.form.get("questionId"))
 
+        ans = data["ans"]
         for i in ans:
             if i["id"] == q_id:
                 i["flag"] += 1
+        data["ans"] = ans
+        writeData(data)
 
         print(time, "Question {} Flagged".format(q_id)) # For log purposes
 
-        answers = sortQuestions(ans)
+        ans = sortQuestions(ans)
+        answers = sortAnswers(ans)
         return render_template("student.html", ans = ans, answers = answers)
 
     elif postOrigin == "like":
-        q_id = request.form.get("questionId")
-        q_id = int(q_id)
+        q_id = int(request.form.get("questionId"))
 
+        ans = data["ans"]
         for i in ans:
             if i["id"] == q_id:
                 i["like"] += 1
+        data["ans"] = ans
+        writeData(data)
         
         print(time, "Answer {} Liked".format(q_id)) # For log purposes
 
-        answers = sortQuestions(ans)
+        ans = sortQuestions(ans)
+        answers = sortAnswers(ans)
         return render_template("student.html", ans = ans, answers = answers)
 
     elif postOrigin == "query":
-        q_id = request.form.get("questionId")
-        q_id = int(q_id)
+        q_id = int(request.form.get("questionId"))
 
+        ans = data["ans"]
         for i in ans:
             if i["id"] == q_id:
                 i["query"] += 1
+        data["ans"] = ans
+        writeData(data)
         
         print(time, "Answer {} Queried".format(q_id)) # For log purposes
 
-        answers = sortQuestions(ans)
+        ans = sortQuestions(ans)
+        answers = sortAnswers(ans)
         return render_template("student.html", ans = ans, answers = answers)
 
 
 @app.route('/tutor', methods=['POST'])
 def post_a():
-    global ans
+    data = loadData()
     answer = request.form.get('answer')
     tutor = request.form.get('tutor')
-    q_id = request.form.get('id')
-    q_id = int(q_id)
+    q_id = int(request.form.get('id'))
     
+    ans = data["ans"]
     for i in ans:
         if i['id'] == q_id:
             i['answer'] = answer
             i['tutor'] = tutor
+    data["ans"] = ans
+    writeData(data)
     
     print(time, "Question {} Answered with '{}' by {}".format(q_id, answer, tutor)) # For log purposes
 
-    answers = sortQuestions(ans)
+    ans = sortQuestions(ans)
+    answers = sortAnswers(ans)
     return render_template('tutor.html', ans = ans, answers = answers)
+
 
 
 #  Run app
